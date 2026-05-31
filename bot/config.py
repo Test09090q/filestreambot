238b421e6e1930009b6d9cbe49ccde398c484464
Test09090q <152406137+Test09090q@users.noreply.github.com>
2026-05-31 23:13:23 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -43,7 +43,7 @@ class Server:
 
 
 class DB:
-    DB_URL = env.get("DB_URL", "postgresql://neondb_owner:npg_ap0hCwtlcD1g@ep-quiet-queen-al0qjnn6-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+    DB_URL = env.get("DB_URL", "")
 
 
 class Util:
