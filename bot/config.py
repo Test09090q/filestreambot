10d56f20647ede3b3cdf944d1c4d6e99af3b9dd8
Test09090q <152406137+Test09090q@users.noreply.github.com>
2026-06-07 23:18:49 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -36,7 +36,7 @@ class Telegram:
 class Server:
     BASE_URL = env.get("BASE_URL", "https://large-guinna-ont-61610454.koyeb.app")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 8080))
+    PORT = int(env.get("PORT", 5432))
     WORKERS_URL = env.get("WORKERS_URL", "")
     WORKERS_URL_2 = env.get("WORKERS_URL_2", "")
     WORKERS_URL_3 = env.get("WORKERS_URL_3", "")
